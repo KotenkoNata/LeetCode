3edee28fb75a4f48885c841d9092658a087dd62c
KotenkoNata <kotenkonata8@gmail.com>
2021-12-23 16:38:27 +0200

    LeetCode 1: Remove Element solution

--- LeetCode0027.js
+++ LeetCode0027.js
@@ -1,21 +1,21 @@
 // TASK https://leetcode.com/problems/remove-element/
 
-const removeElement = function(nums, val) {
-let k = 0;
-let result = [];
+const removeElement = function (nums, val) {
 
-for( let i = 0; i <= nums.length-1; i++){
-  if(nums[i] === val){
-    // result.push("_");
-  }else{
-    k = k + 1;
-    result.unshift(nums[i]);
+  let k = 0;
+
+  for (let i = 0; i <= nums.length - 1; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i = i-1;
+    } else {
+      k = k + 1;
+    }
   }
-}
 
-console.log(typeof result)
-return result;
+
+  return `result ${nums} ${k}`;
 };
 
-console.log(removeElement([3,2,2,3], 3)); //Output: 2, nums = [2,2,_,_]
-console.log(removeElement([0,1,2,2,3,0,4,2], 2)); //Output: 5, nums = [0,1,4,0,3,_,_,_]
+// console.log(removeElement([0,1,2,2,3,0,4,2], 2)); //[0,1,4,0,3]
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); //Output: 5, nums = [0,1,4,0,3,_,_,_]

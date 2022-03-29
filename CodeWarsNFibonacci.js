f9acb0d3bb42c4aa179dcd8417a168ea4b24cf79
KotenkoNata <kotenkonata8@gmail.com>
2022-03-29 19:28:16 +0300

    feat: add solution for N Fibonacci

--- CodeWarsNFibonacci.js
+++ CodeWarsNFibonacci.js
@@ -1,12 +1,28 @@
 // Task - https://www.codewars.com/kata/522551eee9abb932420004a0/train/javascript
 
 function nthFibo(n) {
-  // Return the n-th number in the Fibonacci Sequence
+
+  let result;
+
+  for (let i = 0; i < n; i++) {
+    result = fib(i)
+  }
+  return result;
 }
 
+let fib = function (n) {
 
-console.log(nthFibo(1))//0,"1-st Fibo
+  if (n === 0) return 0;
+  if (n === 1) return 1;
 
-console.log(nthFibo(2))//1,"2-nd Fibo");
+  return fib(n - 1) + fib(n - 2)
+};
 
-console.log(nthFibo(3))//1,"3-rd Fibo");
+
+// console.log(nthFibo(1))//0,"1-st Fibo
+
+// console.log(nthFibo(2))//1,"2-nd Fibo");
+
+// console.log(nthFibo(3))//1,"3-rd Fibo");
+
+console.log(nthFibo(4))//2,"4-th Fibo");

2bc503da55130826552f439dba84b9518c67f643
KotenkoNata <kotenkonata8@gmail.com>
2022-03-29 12:45:30 +0300

    feat: add solution for 509. Fibonacci Number

---
+++ CodeWarsNFibonacci.js
@@ -0,0 +1,12 @@
+// Task - https://www.codewars.com/kata/522551eee9abb932420004a0/train/javascript
+
+function nthFibo(n) {
+  // Return the n-th number in the Fibonacci Sequence
+}
+
+
+console.log(nthFibo(1))//0,"1-st Fibo
+
+console.log(nthFibo(2))//1,"2-nd Fibo");
+
+console.log(nthFibo(3))//1,"3-rd Fibo");

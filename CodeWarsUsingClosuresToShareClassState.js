eb3b0c71487c7737be3532054a87aa4203f0569d
KotenkoNata <kotenkonata8@gmail.com>
2022-03-29 14:55:39 +0300

    feat: add solution for N-th Fibonacci

---
+++ CodeWarsUsingClosuresToShareClassState.js
@@ -0,0 +1,12 @@
+// Task - https://www.codewars.com/kata/53583765d5493bfdf5001b35/train/javascript
+
+class Cat {
+  constructor(name, weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+}
+
+Cat.prototype.averageWeight = function () {
+  
+}

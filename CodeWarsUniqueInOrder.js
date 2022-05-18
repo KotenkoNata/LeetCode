15ec28e8f73d76bf6399f3650d5ea5169915e579
KotenkoNata <kotenkonata8@gmail.com>
2022-05-18 15:37:13 +0300

    feat: add solution for Unique In Order

---
+++ CodeWarsUniqueInOrder.js
@@ -0,0 +1,15 @@
+const uniqueInOrder = function (iterable) {
+  let result = [];
+
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      result.push(iterable[i]);
+    }
+  }
+
+  return result;
+}
+
+// console.log(uniqueInOrder('AAAABBBCCDAABBB')) //['A','B','C','D','A','B']
+
+console.log(uniqueInOrder([1, 2, 3, 3])) //[ 1, 2, 3 ]

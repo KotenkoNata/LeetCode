21f2440ac96b96754bbefafd6c457c379affcf03
KotenkoNata <kotenkonata8@gmail.com>
2022-05-23 19:04:12 +0300

    feat: add solution for Calculating with Functions

---
+++ CodeWarsCalculatingFunctions.js
@@ -0,0 +1,73 @@
+function returnNumberOrFunction(number, operation) {
+  if (!operation) {
+    return number;
+  }
+  return operation(number);
+}
+
+function zero(operation) {
+  return returnNumberOrFunction(0, operation);
+}
+
+function one(operation) {
+  return returnNumberOrFunction(1, operation);
+}
+
+function two(operation) {
+  return returnNumberOrFunction(2, operation);
+}
+
+function three(operation) {
+  return returnNumberOrFunction(3, operation);
+}
+
+function four(operation) {
+  return returnNumberOrFunction(4, operation);
+}
+
+function five(operation) {
+  return returnNumberOrFunction(5, operation);
+}
+
+function six(operation) {
+  return returnNumberOrFunction(6, operation);
+}
+
+function seven(operation) {
+  return returnNumberOrFunction(7, operation);
+}
+
+function eight(operation) {
+  return returnNumberOrFunction(8, operation);
+}
+
+function nine(operation) {
+  return returnNumberOrFunction(9, operation);
+}
+
+function plus(a) {
+  return function (b) {
+    return b + a;
+  }
+}
+
+function minus(a) {
+  return function (b) {
+    return b - a;
+  }
+}
+
+function times(a) {
+  return function (b) {
+    return b * a;
+  }
+}
+
+function dividedBy(a) {
+  return function (b) {
+    return Math.floor(b / a);
+  }
+}
+
+console.log(seven(times(five()))); //35
+console.log(four(plus(nine())));

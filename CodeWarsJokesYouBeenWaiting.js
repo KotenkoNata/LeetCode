eb2f3ed318ed5c5a35d05e737de3c7f5e63f91ad
KotenkoNata <kotenkonata8@gmail.com>
2022-06-03 20:37:50 +0300

    feat: add solution for Jokes you've been 'awaiting' for ... promise

---
+++ CodeWarsJokesYouBeenWaiting.js
@@ -0,0 +1,23 @@
+// Task - https://www.codewars.com/kata/5a353a478f27f244a1000076/train/javascript
+
+function sayJoke(apiUrl, jokeId) {
+  return fetch(apiUrl)
+    .then(response =>
+      response.json()
+    )
+    .then(json => {
+      for (let joke of json.jokes) {
+        if (joke.id === jokeId) {
+          return {
+            saySetup: () => joke.setup,
+            sayPunchLine: () => joke.punchLine
+          };
+        }
+      }
+      throw new Error(`No jokes found id: ${jokeId}`);
+    }).catch(error => {
+      throw (error instanceof TypeError) ? new Error(`No jokes at url: ${apiUrl}`) : error
+    })
+}
+
+

55ab9d0da78d38d9cac374b9abd27bf8dd5c7799
KotenkoNata <kotenkonata8@gmail.com>
2022-06-03 19:04:08 +0300

    feat: add solution for Using closures to share class state

--- CodeWarsUsingClosuresToShareClassState.js
+++ CodeWarsUsingClosuresToShareClassState.js
@@ -1,12 +1,30 @@
 // Task - https://www.codewars.com/kata/53583765d5493bfdf5001b35/train/javascript
 
 class Cat {
+  static cats = [];
+
   constructor(name, weight) {
-    this.name = name;
-    this.weight = weight;
+
+    if (!name) {
+      throw new Error("Please add name")
+    }
+    if (!weight) {
+      throw new Error("Please add weight")
+    }
+
+    Object.defineProperty(this, 'name', {get: () => name});
+    Object.defineProperty(this, 'weight', {get: () => weight, set: value => weight = value});
+
+    Cat.cats.push(this)
+  }
+
+  static averageWeight() {
+    return Cat.cats.reduce((acc, cat) => (acc + cat.weight), 0) / Cat.cats.length
   }
 }
 
-Cat.prototype.averageWeight = function () {
-  
-}
+
+fluffy = new Cat('fluffy', 15);
+garfield = new Cat('garfield', 25);
+
+console.log(Cat.averageWeight())

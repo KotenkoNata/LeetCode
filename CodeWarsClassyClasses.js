74d057642e2f4eb02b429109c4940b60f02ee333
KotenkoNata <kotenkonata8@gmail.com>
2022-06-15 18:48:55 +0300

    feat: add solution for Classy Classes

---
+++ CodeWarsClassyClasses.js
@@ -0,0 +1,18 @@
+// Task - https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+
+  }
+
+  get info() {
+    return `${this.name}s age is ${this.age}`
+  }
+}
+
+
+let john = new Person('john', 34)
+
+console.log(john.info);

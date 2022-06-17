5aa3bb462504caadef449675dbd0067ddc485b03
KotenkoNata <kotenkonata8@gmail.com>
2022-06-17 19:48:59 +0300

    feat: add solution for Remove exclamation marks

---
+++ CodeWarsRemoveExclamationMarks.js
@@ -0,0 +1,7 @@
+// Task - https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/gi, '');
+}
+
+console.log(removeExclamationMarks("Hello World!!!"))

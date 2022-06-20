ca8767fd45b37d79b6db4985e41fc3fe69079776
KotenkoNata <kotenkonata8@gmail.com>
2022-06-20 15:32:41 +0300

    feat: add solution for rot13

---
+++ CodeWarsRot13.js
@@ -0,0 +1,9 @@
+// Task - https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
+
+function rot13(message) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const ciplher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+  return message.replace(/[a-z]/gi, letter => ciplher[alphabet.indexOf(letter)])
+}
+
+console.log(rot13("Test")) //"grfg"

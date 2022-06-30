553c9cdd0adf0b70adae9399d320dfcc5572ef3d
KotenkoNata <kotenkonata8@gmail.com>
2022-06-30 11:44:09 +0300

    feat: add solution for If you can't sleep, just count sheep!!

---
+++ CodeWarsIfYouCantSleep.js
@@ -0,0 +1,11 @@
+// Task - https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/javascript
+
+const countSheep = function (num) {
+  let result = ''
+  for (let i = 1; i <= num; i++) {
+    result += `${i} sheep...`
+  }
+  return result;
+}
+
+console.log(countSheep(5)) //"1 sheep..."

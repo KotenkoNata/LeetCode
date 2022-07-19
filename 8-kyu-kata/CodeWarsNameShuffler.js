f721389fce7dbc20c7889eb933b0047a1a3de160
KotenkoNata <kotenkonata8@gmail.com>
2022-07-19 10:41:16 +0300

    feat: add solution for Name Shuffler

---
+++ 8-kyu-kata/CodeWarsNameShuffler.js
@@ -0,0 +1,7 @@
+// TAsk - https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
+
+function nameShuffler(str) {
+  return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane')) // 'McClane john'

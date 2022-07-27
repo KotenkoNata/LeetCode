47bc5ebb706f76f3dfb56e81e77de14f5e1ab9b3
KotenkoNata <kotenkonata8@gmail.com>
2022-07-27 11:55:24 +0300

    feat: add solution for Beginner Series #4 Cockroach

---
+++ 8-kyu-kata/CodeWarsBegginnerSeriesCockroach.js
@@ -0,0 +1,8 @@
+// Task - https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.777778)
+}
+
+console.log(cockroachSpeed(1.08)) //30
+console.log(cockroachSpeed(2.3668939522037937))

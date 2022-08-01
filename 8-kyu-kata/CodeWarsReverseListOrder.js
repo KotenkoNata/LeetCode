094f15fd20c050d7f2bd5550df2b21c69f86d0bb
KotenkoNata <kotenkonata8@gmail.com>
2022-08-01 14:59:23 +0300

    feat: add solution for Reverse List Order

---
+++ 8-kyu-kata/CodeWarsReverseListOrder.js
@@ -0,0 +1,7 @@
+// TAsk - https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/javascript
+
+function reverseList(list) {
+  return list.reverse();
+}
+
+console.log(reverseList([1, 2, 3, 4]))

ead44ac8bf19af5bc1bf2a653be18c02a4678ee4
KotenkoNata <kotenkonata8@gmail.com>
2022-08-02 11:59:13 +0300

    feat: add solution for Is this a triangle?

---
+++ 7-kyu-kata/CodeWarsIsThisTriangle.js
@@ -0,0 +1,11 @@
+// TAsk - https://www.codewars.com/kata/56606694ec01347ce800001b/train/javascript
+
+function isTriangle(a, b, c) {
+
+  if (a + b > c && a + c > b && b + c > a) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isTriangle(7, 2, 2)) //true
